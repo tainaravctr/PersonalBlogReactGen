ca9ab2ce1774e9aa0a705ac3f5b01941f93e05f3
tainaravctr <tainarisvictoria@gmail.com>
2023-10-03 16:19:07 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './paginas/Home';
+import './App.css';
+import React from 'react';
+
+function App() {
+ 
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
